9f859dc93206d59688b12b11a3fe8646101f4d1b
Gracie McGuire <m.graciemcguire@gmail.com>
2018-11-18 13:28:58 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -16,11 +16,3 @@ def run_guessing_game(tale)
    end
 end
 
-def tale 
-  "There was once an ogre who had no friends,
-He lived out in the swamp for the longest __while__,
-Living out his days in quiet contemplation,
-He really didn't tolerate the presence of __rand__oms,
-Until an annoying donkey once came along,
-And started to __break__ his brain with madness."
-end
